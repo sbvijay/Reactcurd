769faba95323ba297b66148fc39f29ffbc808283
sbvijay <vijay@sketchbrahma.com>
2019-08-19 19:48:41 +0530

    edit

--- src/Forms/AddEmpForm.js
+++ src/Forms/AddEmpForm.js
@@ -2,6 +2,12 @@ import React, { useState } from 'react'
 
 const AddEmpForm = props => {
 
+    const empsData = [
+        { id: 1, empid: 123, empname: 'vijay' },
+        { id: 2, empid: 124, empname: 'sachin' },
+        { id: 3, empid: 111, empname: 'kumar' },
+    ]
+
     const initialFormState = { id: null, empid: '', empname: '' }
     const [emp, setEmp] = useState(initialFormState)
 
@@ -15,28 +21,34 @@ const AddEmpForm = props => {
         const string = String.fromCharCode(keyCode)
         const regex = /[0-9,]|\./
         if (!regex.test(string)) {
-          event.returnValue = false
-          if (event.preventDefault) event.preventDefault()
+            event.returnValue = false
+            if (event.preventDefault) event.preventDefault()
         }
-      }
+    }
 
     return (
         <form onSubmit={event => {
             event.preventDefault()
-            if (!emp.empid || !emp.empname) return
-            props.addEmp(emp)
-            setEmp(initialFormState)
+            var index = empsData.findIndex(x => x.empid == emp.empid)
+            if (index === -1) {
+                if (!emp.empid || !emp.empname) return
+                props.addEmp(emp)
+                setEmp(initialFormState)
+            }
+            else {
+                alert("Emp id present");
+            }
         }}>
-            
-        <div className="custom-input">
-            <label>EmpId</label>
-            <input type="text" name="empid" value={emp.empid} onChange={handleInputChange} onKeyPress={validateNumber} />
-        </div>
-        <div className="custom-input">
-            <label>EmpName</label>
-            <input type="text" name="empname" value={emp.empname} onChange={handleInputChange} />
-        </div>
-        <button className="custom-btn">Add New Emp </button>
+
+            <div className="custom-input">
+                <label>EmpId</label>
+                <input type="text" name="empid" value={emp.empid} onChange={handleInputChange} onKeyPress={validateNumber} />
+            </div>
+            <div className="custom-input">
+                <label>EmpName</label>
+                <input type="text" name="empname" value={emp.empname} onChange={handleInputChange} />
+            </div>
+            <button className="custom-btn">Add New Emp </button>
         </form>
     )
 }
